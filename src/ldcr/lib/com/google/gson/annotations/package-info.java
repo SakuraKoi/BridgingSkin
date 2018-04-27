c7885b31c8743a064c70aff45335bde6d96279fc
Ldcr <starsky2013@126.com>
2018-04-27 10:37:24 +0800

    Remove gson lib & use LdcrUtils

--- src/ldcr/lib/com/google/gson/annotations/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package provides annotations that can be used with {@link ldcr.lib.com.google.gson.Gson}.
- * 
- * @author Inderjeet Singh, Joel Leitch
- */
-package ldcr.lib.com.google.gson.annotations;
